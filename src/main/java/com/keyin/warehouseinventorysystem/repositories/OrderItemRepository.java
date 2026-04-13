c135503d60153a0502d5317e938b597872492e2f
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-04-13 19:03:10 -0230

    added repositories, CRUD will be handled by springboot

---
+++ src/main/java/com/keyin/warehouseinventorysystem/repositories/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.keyin.warehouseinventorysystem.repositories;
+
+import com.keyin.warehouseinventorysystem.models.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class OrderItemRepository {
+
+}
