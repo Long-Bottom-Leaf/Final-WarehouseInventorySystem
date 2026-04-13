a37c5a7df41259327af86dc30909bd4b9b10e263
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-04-13 19:11:00 -0230

    woops fixed a mistake in two repo layers

--- src/main/java/com/keyin/warehouseinventorysystem/repositories/OrderItemRepository.java
+++ src/main/java/com/keyin/warehouseinventorysystem/repositories/OrderItemRepository.java
@@ -3,6 +3,6 @@ package com.keyin.warehouseinventorysystem.repositories;
 import com.keyin.warehouseinventorysystem.models.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class OrderItemRepository {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
 }
--- src/main/java/com/keyin/warehouseinventorysystem/repositories/OrderRepository.java
+++ src/main/java/com/keyin/warehouseinventorysystem/repositories/OrderRepository.java
@@ -3,6 +3,6 @@ package com.keyin.warehouseinventorysystem.repositories;
 import com.keyin.warehouseinventorysystem.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class OrderRepository {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
