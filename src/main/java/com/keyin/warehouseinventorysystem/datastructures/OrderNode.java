7bfd85fdefbeeea0e85134a9ed73942d7ad1d9bf
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-04-14 19:18:56 -0230

    BST should be complete

---
+++ src/main/java/com/keyin/warehouseinventorysystem/datastructures/OrderNode.java
@@ -0,0 +1,17 @@
+package com.keyin.warehouseinventorysystem.datastructures;
+
+import org.w3c.dom.Node;
+import com.keyin.warehouseinventorysystem.models.Order;
+
+public class OrderNode {
+
+    public Order data;
+    public OrderNode left;
+    public OrderNode right;
+
+    public OrderNode(Order data) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
