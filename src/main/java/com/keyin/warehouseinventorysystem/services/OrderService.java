da66425b4f70f50c5c81fd3f9e37da7e75db3c37
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-04-14 13:59:51 -0230

    working on service layer

---
+++ src/main/java/com/keyin/warehouseinventorysystem/services/OrderService.java
@@ -0,0 +1,12 @@
+package com.keyin.warehouseinventorysystem.services;
+
+import com.keyin.warehouseinventorysystem.models.Order;
+import com.keyin.warehouseinventorysystem.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class OrderService {
+
+}
